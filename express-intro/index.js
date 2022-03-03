3b7e3d7db91b23e41c354abb6ff5eaf3bb2379f8
adnankhateeb <adnankhateeb21@gmail.com>
2022-03-03 19:39:58 +0530

    Update index.js

--- express-intro/index.js
+++ express-intro/index.js
@@ -7,27 +7,25 @@ app.get("", (req, res) => {
     res.send("hello")
 })
 
-let books = [{
-    name: "One Hundred Years Of Solitude",
-    author: "Gabriel Garcia Marquez"
-},
-{
-    name: "Digital Fortress",
-    author: "Stephen Brown"
-},
-{
-    name: "Crime And Punishment",
-    author: "Fyodor Dostoevsky"
-},
-{
-    name: "Notes from Underground",
-    author: "Fyodor Dostoevsky"
-}]
 
 app.get("/books", (req, res) => {
-    res.send(JSON.stringify(books))
+    res.json([{
+        "name": "One Hundred Years Of Solitude",
+        "author": "Gabriel Garcia Marquez"
+    },
+    {
+        "name": "Digital Fortress",
+        "author": "Stephen Brown"
+    },
+    {
+        "name": "Crime And Punishment",
+        "author": "Fyodor Dostoevsky"
+    },
+    {
+        "name": "Notes from Underground",
+        "author": "Fyodor Dostoevsky"
+    }]
+    )
 })
 
-app.listen(4000, () => {
-    console.log("listening on port 4000");
-});
+app.listen(4000);
