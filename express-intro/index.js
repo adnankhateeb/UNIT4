a8012c4be6bc03236409835926f5917d8c5a95ca
adnankhateeb <adnankhateeb21@gmail.com>
2022-03-03 19:48:13 +0530

    addedJSONasImport

--- express-intro/index.js
+++ express-intro/index.js
@@ -1,6 +1,7 @@
 
 const express = require('express')
 const app = express();
+const books = require('./books.json')
 
 
 app.get("", (req, res) => {
@@ -9,23 +10,7 @@ app.get("", (req, res) => {
 
 
 app.get("/books", (req, res) => {
-    res.json([{
-        "name": "One Hundred Years Of Solitude",
-        "author": "Gabriel Garcia Marquez"
-    },
-    {
-        "name": "Digital Fortress",
-        "author": "Stephen Brown"
-    },
-    {
-        "name": "Crime And Punishment",
-        "author": "Fyodor Dostoevsky"
-    },
-    {
-        "name": "Notes from Underground",
-        "author": "Fyodor Dostoevsky"
-    }]
-    )
+    res.send(books)
 })
 
 app.listen(4000);
